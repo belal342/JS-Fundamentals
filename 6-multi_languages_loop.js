7baa950361a2f810f7a6170ec91d5cc3643a92a9
belal <blalsmyr512@gmail.com>
2025-08-28 01:04:21 +0300

    6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,6 @@
-const arr=['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let i=0;
-while(i<arr.length){
-    console.log(arr[i]); 
-    i++;
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
 }
